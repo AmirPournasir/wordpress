7b192f864b8ed23a6d9129ab83b27ebef7b66456
amirpournasir <fixhubteam@gmail.com>
2023-10-23 20:38:48 +0330

    post-type.php

--- post-type.php
+++ post-type.php
@@ -38,3 +38,34 @@ function custom_post_type() {
 }
 
 add_action('init', 'custom_post_type');
+
+
+// Register a custom taxonomy
+function custom_taxonomy() {
+    $labels = array(
+        'name' => _x('Custom Taxonomy', 'taxonomy general name', 'your-text-domain'),
+        'singular_name' => _x('Custom Term', 'taxonomy singular name', 'your-text-domain'),
+        'search_items' => __('Search Custom Terms', 'your-text-domain'),
+        'all_items' => __('All Custom Terms', 'your-text-domain'),
+        'parent_item' => __('Parent Custom Term', 'your-text-domain'),
+        'parent_item_colon' => __('Parent Custom Term:', 'your-text-domain'),
+        'edit_item' => __('Edit Custom Term', 'your-text-domain'),
+        'update_item' => __('Update Custom Term', 'your-text-domain'),
+        'add_new_item' => __('Add New Custom Term', 'your-text-domain'),
+        'new_item_name' => __('New Custom Term Name', 'your-text-domain'),
+        'menu_name' => __('Custom Taxonomy', 'your-text-domain'),
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'custom-taxonomy'),
+    );
+    
+    register_taxonomy('custom_taxonomy', 'custom_post_type', $args);
+}
+
+add_action('init', 'custom_taxonomy');
